f84db41bcaa8b5bdd607bb9dc0479c5bea57566f
MuhamadAbsorDwiyana <dwiyanaabsor@gmail.com>
2024-08-09 13:20:42 +0700

    add GORM config

--- Configs/db.go
+++ Configs/db.go
@@ -1,5 +1,6 @@
 package configs
 
+// Import modules gorm dan gotenv
 import (
 	"fmt"
 	"log"
@@ -10,13 +11,18 @@ import (
 	"gorm.io/gorm"
 )
 
+// Set variable for gorm DB
 var DB *gorm.DB
 
+// Function for init Database
 func InitDB() {
+	// get .env content use godotenv
 	godotenv.Load()
 
+	// Set err as error type data
 	var err error
 
+	// Set env info + set database
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
@@ -25,10 +31,13 @@ func InitDB() {
 		os.Getenv("DB_DBNAME"),
 	)
 
+	// Check database connecton
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// If error return sql error log
 	if err != nil {
 		return
 	}
 
+	// If success connect to database
 	log.Printf("Akses ke database %s berhasil", os.Getenv("DB_DBNAME"))
 }
