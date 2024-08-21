2d9acf347de8c7ecfc07fdb854893e97f9737c08
MuhamadAbsorDwiyana <dwiyanaabsor@gmail.com>
2024-08-21 23:31:31 +0700

    int(8) to uint

--- Models/Visitor.go
+++ Models/Visitor.go
@@ -7,8 +7,8 @@ import (
 type Visitor struct {
 	gorm.Model        // gorm default record (ID, CreatedAt, UpdatedAt, DeletedAt)
 	Avatar     string `json:"avatar" gorm:"type:varchar(255)"`
-	Name       string `json:"name" gorm:"type:varchar(100);not null"`
-	Identity   string `json:"identity" gorm:"type:varchar(20);unique;not null"`
-	Address    string `json:"address" gorm:"type:text;not null"`
-	Age        uint8  `json:"age" gorm:"type:int(3);not null"`
+	Name       string `json:"name" gorm:"type:varchar(100)"`
+	Identity   string `json:"identity" gorm:"type:varchar(20);unique"`
+	Address    string `json:"address" gorm:"type:text"`
+	Age        uint8  `json:"age" gorm:"type:uint"`
 }
